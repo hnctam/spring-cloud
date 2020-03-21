3277606f1d616c4693e701801d618322ff62aaa4
Nguyen Chinh Tam Ho <tamhnc@hitachiconsulting.com>
2020-03-21 09:20:12 -0500

    Upgrade SpringCloud version to be able to launch the Eureka server with SpringBoot 2.2.5

--- dcp-validator/src/main/java/com/ami/pcf/MainApplication.java
+++ dcp-validator/src/main/java/com/ami/pcf/MainApplication.java
@@ -2,8 +2,10 @@ package com.ami.pcf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MainApplication {
 
 	public static void main(String[] args) {
