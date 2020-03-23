5c1f6601f16ecc710d2825bc46e545d242978a72
Nguyen Chinh Tam Ho <tamhnc@hitachiconsulting.com>
2020-03-23 13:49:34 -0500

    Remove warnings from pom files

--- dcp-validator-service/src/main/java/com/ami/pcf/dcp/service/DcpValidatorService.java
+++ dcp-validator-service/src/main/java/com/ami/pcf/dcp/service/DcpValidatorService.java
@@ -1,19 +1,9 @@
 package com.ami.pcf.dcp.service;
 
-import java.util.Collections;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.ami.pcf.dcp.common.ServiceContext;
 import com.ami.pcf.dcp.domain.DataCollectionPlan;
-import com.ami.pcf.dcp.dto.PageResponse;
-import com.ami.pcf.dcp.repository.DcpRepo;
-import com.ami.pcf.dcp.util.AppUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
